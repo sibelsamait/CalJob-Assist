dc7b4553ae3734330122d41e28f5ab69abb827e6
Love for All <lovely5.techserv@gmail.com>
2026-07-16 16:41:11 +0000

    refactor: ultimos arreglos copilot #8

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"0e85asKH1JVzZZ934govbsv9DiHlNZ6TAcA0xZ4gcFc=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"xCuO3iT0MqWVM+BxgndCol+GBB5oIAH+iqNi07/HcQI=\"}"
